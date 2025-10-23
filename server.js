0d5ac961577df1c8e9532cb9a26d8a36af03c1f6
Gustavo <yaantavaresdossantos2003@gmail.com>
2025-10-23 09:25:08 -0300

    put

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ server.post('/personagem', (req, reply) => {
     return 'Deu certo!'
 })
 
+server.put('/personagem/:nome', (req, reply) => {
+    const nome = req.params.nome;
+// Faça uma lógica para verificar se ja tem um personagem
+// Com esse nome. ( DENTRO DO ENDPOINT DO POST )
+    let valido = false;
+    for (let index in lista) {
+        if (nome === lista[index].nome) {
+            lista[index] = req.body;
+            break
+        }
+    }
+
+    return lista
+})
+
 server.listen(
     {port: 3000}
 )
